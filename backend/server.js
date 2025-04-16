2c5129dcf82b9c5e41dc9f4402e08d7918fdf664
“rutaiogit config --global user.email youremail@gmail.comrutadanyt@gmail.com <rutadanyt@gmail.com>
2025-04-16 15:54:50 +0100

    add mongo

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,30 @@
 const express = require('express');
 const cors = require('cors');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
 const userRoutes = require('./routes/userRoutes');
 const productRoutes = require('./routes/productRoutes');
 const messagesRoutes = require('./routes/messagesRoutes');
 
-const app = express();
-const PORT = 3000;
+dotenv.config();
+const PORT = process.env.PORT || 3000;
 
+const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/api/users', userRoutes);
 app.use('/api/products', productRoutes);
 app.use('/api/messages', messagesRoutes);
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('Connected to MongoDB Atlas');
+  })
+  .catch((error) => {
+    console.log('Error connecting to MongoDB Atlas', error);
+  });
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
