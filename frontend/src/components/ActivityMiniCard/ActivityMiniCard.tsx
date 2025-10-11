a33ecc50ee9edaf8a168eb85cd4500adc6ee32a9
“rutaiogit config --global user.email youremail@gmail.comrutadanyt@gmail.com <rutadanyt@gmail.com>
2025-10-11 19:08:56 +0100

    styling fixes for ActivityMiniCard

--- frontend/src/components/ActivityMiniCard/ActivityMiniCard.tsx
+++ frontend/src/components/ActivityMiniCard/ActivityMiniCard.tsx
@@ -1,21 +1,21 @@
 import { Activity } from '../../types/types';
 import './activity-mini-card.css';
+import { Link } from 'react-router-dom';
 
 interface ActivityMiniCardProps {
   activity: Activity;
 }
 
+// fix the link: make the whole card clickable
 export const ActivityMiniCard = ({ activity }: ActivityMiniCardProps) => {
   return (
-    <div className="activity-mini-card">
+    <Link to={`/activities/${activity.slug}`} className="activity-mini-card">
       <img
         src={activity.image}
         alt="Activity cover image"
         className="activity-mini-card-image"
       />
-      <h4 className="activity-mini-card-title">
-        <a href={`/activities/${activity.slug}`}>{activity.title}</a>
-      </h4>
-    </div>
+      <h4 className="activity-mini-card-title">{activity.title}</h4>
+    </Link>
   );
 };
